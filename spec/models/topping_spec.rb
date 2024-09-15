458506a7b4b40ef3745bc9c6bb06a02b71498458
paullemus1997 <paullemus1997@penguin>
2024-09-15 15:40:57 -0700

    spec: added unit spec for Topping model
    Covers the following Story:
    - It should not allow me to enter duplicate toppings

--- spec/models/topping_spec.rb
+++ spec/models/topping_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Topping, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'validates presence of name' do
+      topping = Topping.new(name: '')
+      expect(topping).not_to be_valid
+    end
+
+    it 'validates uniqueness of name' do
+      Topping.create!(name: 'Pepperoni')
+      duplicate_topping = Topping.new(name: 'Pepperoni')
+      expect(duplicate_topping).not_to be_valid
+    end
+  end
 end
