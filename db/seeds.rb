b92ec6ed72bce3fbf8da6e996148fd1e42d798db
paullemus1997 <paullemus1997@penguin>
2024-09-16 11:40:54 -0700

    feat: add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,33 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Clear existing data
+Topping.destroy_all
+Pizza.destroy_all
+User.destroy_all
+
+# Create Toppings
+cheese = Topping.create!(name: 'Cheese')
+tomato = Topping.create!(name: 'Tomato')
+pepperoni = Topping.create!(name: 'Pepperoni')
+bacon = Topping.create!(name: 'Bacon')
+mushrooms = Topping.create!(name: 'Mushrooms')
+olives = Topping.create!(name: 'Olives')
+onions = Topping.create!(name: 'Onions')
+
+# Create Pizzas
+Pizza.create!(name: 'Margherita', toppings: [cheese, tomato])
+Pizza.create!(name: 'Pepperoni', toppings: [cheese, tomato, pepperoni])
+Pizza.create!(name: 'Meat Lovers', toppings: [cheese, bacon, pepperoni])
+Pizza.create!(name: 'Veggie Delight', toppings: [cheese, mushrooms, olives, onions])
+
+# Create Users
+store_owner = User.create!(
+  email: 'owner@pizza.com',
+  password: 'i<3pizza',
+  role: 'store_owner'
+)
+
+pizza_chef = User.create!(
+  email: 'chef@pizza.com',
+  password: 'i<3pizza',
+  role: 'pizza_chef'
+)
+
