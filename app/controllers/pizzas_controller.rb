68f008e7eac16e2b17ff2aa2aae4017ad17a9b82
paullemus1997 <paullemus1997@penguin>
2024-09-16 01:22:40 -0700

    feat: added create new pizza with toppings
    Uses existing toppings
    
    Resolves:
    It should allow me to create a new pizza and add toppings to it

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -6,9 +6,33 @@ class PizzasController < ApplicationController
   end
 
   def new
+    @pizza = Pizza.new
+    @toppings = Topping.all
+  end
+
+  def create
+    @pizza = Pizza.new(pizza_params)
+    if @pizza.save
+      redirect_to pizzas_path, notice: 'Pizza was successfully created.'
+    else
+      @toppings = Topping.all
+      render :new
+    end
+  end
+
+  def edit
+    @toppings = Topping.all
   end
 
   private
+
+  def pizza_params
+    params.require(:pizza).permit(:name, topping_ids: [])
+  end
+
+  def set_pizza
+    @pizza = Pizza.find(params[:id])
+  end
 
   def authorize_pizza_chef
     unless current_user&.role == 'pizza_chef' || current_user&.role == 'store_owner'
