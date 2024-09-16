d0a822d4b0d1cc9129f6a64d3e4e4c266a752802
paullemus1997 <paullemus1997@penguin>
2024-09-15 17:36:56 -0700

    spec: added new topping spec

--- spec/features/manage_toppings_spec.rb
+++ spec/features/manage_toppings_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe 'Manage Toppings', type: :feature do
     expect(page).to have_content('Pepperoni')
     expect(page).to have_content('Mushroom')
   end
+
+  scenario 'add a new topping' do
+    visit new_topping_path
+
+    fill_in 'Name', with: 'Bell Peppers'
+    click_button 'Create Topping'
+
+    expect(page).to have_content('Bell Peppers')
+    expect(Topping.count).to eq(1)
+  end
 end
