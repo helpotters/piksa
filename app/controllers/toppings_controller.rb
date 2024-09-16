ce08a74a63d6ff93f08b58cf8278f753feeef17b
paullemus1997 <paullemus1997@penguin>
2024-09-15 17:37:16 -0700

    feat: new topping form
    Resolves:
    It should allow me to add a new topping

--- app/controllers/toppings_controller.rb
+++ app/controllers/toppings_controller.rb
@@ -2,4 +2,23 @@ class ToppingsController < ApplicationController
   def index
     @toppings = Topping.all
   end
+
+  def new
+    @topping = Topping.new
+  end
+  def create
+    @topping = Topping.new(topping_params)
+    if @topping.save
+      redirect_to toppings_path, notice: 'Topping was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def topping_params
+    params.require(:topping).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :toppings, only: [:index, :show]  # Defines both index and show routes
+  resources :toppings, only: [:index, :show, :new, :create]
 end
